2a5d7be280941f373c7dd0fb55f34fefb2f5f541
qwattash <mzz.lrd@gmail.com>
2014-09-26 19:13:14 +0200

    Closes #9

--- tests/test_module_inclusion/scope.mk
+++ tests/test_module_inclusion/scope.mk
@@ -2,6 +2,8 @@
 # declare here namespace in ./src_1/namespace_1
 #
 
+TEST_SCOPE_PATH_BASE := $(call anrem-current-path)
+
 # use custom name to avoid conflict among ./src_2/confilcts/namespace_1 and ./src_1/namespace_1
 $(call anrem-ns-register, ./src_1/namespace_1, src_1_ns_1)
 
